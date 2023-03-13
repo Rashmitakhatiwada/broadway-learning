e7eaa971414a7a2fd8d2ac24659d42a847912a7b
Peshal Bhandari <rasmitakhatiwada2@gmail.com>
2023-03-13 10:46:41 +0545

    main

--- main.py
+++ main.py
@@ -10,36 +10,32 @@ def print_hi(name):
 
 
 # Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
 
 print("hello world")
-print("checking")
-
-print("aa")
-
-Add commit push
-Git init
-Git add . [Instead  of . We can also use “—all” or <file names>]
-Git commit -m “…msz..”
-Git push -u origin <branch name/ master>
-
-Created. A GitHub repo
-Git remote add origin <ssh link>
-
-Ssh-keygen  -t ed25519 -C “email id” [created ssh  in local]
-
-Gi config global user.name “username” [set git user name to initalize who commited]
-Git config ==global user.email “email”
 
 
-New branch creating
-Git branch <new name>
-
-To check
-git branch
-
-To switch to other branch
-Git checkout <new branch name>
+# Add commit push
+# Git init
+# Git add . [Instead  of . We can also use “—all” or <file names>]
+# Git commit -m “…msz..”
+# Git push -u origin <branch name/ master>
+#
+# Created. A GitHub repo
+# Git remote add origin <ssh link>
+#
+# Ssh-keygen  -t ed25519 -C “email id” [created ssh  in local]
+#
+# Gi config global user.name “username” [set git user name to initalize who commited]
+# Git config ==global user.email “email”
+#
+#
+# New branch creating
+# Git branch <new name>
+#
+# To check
+# git branch
+#
+# To switch to other branch
+# Git checkout <new branch name>
